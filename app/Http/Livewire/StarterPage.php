4dbada65aca4b38a44abe7d29b855923f9378237
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-05-10 15:17:49 +0700

    Add starter page livewire :construction:

---
+++ app/Http/Livewire/StarterPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class StarterPage extends Component
+{
+    public function render()
+    {
+        return view('starter-page');
+    }
+}
