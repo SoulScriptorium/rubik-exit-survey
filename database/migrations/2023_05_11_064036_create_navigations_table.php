70019c7eb2e66477d003355c78b9b46cb5321a14
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-05-11 15:22:06 +0700

    Add pages & navigation table :dolphin: :card_file_box:

---
+++ database/migrations/2023_05_11_064036_create_navigations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order');
+            $table->timestamps();
+        });
+
+        Schema::create('question_navigation', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('next_page')->constrained(table: 'pages')
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('prev_page')->constrained(table: 'pages')
+                ->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('question_navigation');
+        Schema::dropIfExists('pages');
+    }
+};
