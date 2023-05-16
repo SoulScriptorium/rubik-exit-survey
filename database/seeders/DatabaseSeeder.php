3d4c921fe0bfc2e7384e0228bd06f28d2f001b3f
Fikry C. Farenza <vickyfarenza@gmail.com>
2023-05-16 14:13:03 +0700

    Make seeder for survey simulation :seed:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Answer;
+use App\Models\Page;
+use App\Models\Question;
+use App\Models\Responder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Create 5 responders
+        $responders = Responder::factory()->count(5)->create();
+
+        // Survey
+        Page::factory(5)->has(
+            Question::factory(3)->has(
+                Answer::factory(2)->hasAttached($responders, [
+                    'text' => fake()->sentence,
+                    'checked' => fake()->boolean,
+                ])->state(function () {
+                    return ['next_page' => random_int(0, 1) == 1 ? Page::inRandomOrder()->first()->id : null];
+                })
+            ))->create();
     }
 }
